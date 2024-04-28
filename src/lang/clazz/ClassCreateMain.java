6bcb7b102925ffd735937f6296a3cd0129e0b8e8
김승현 <dsagom@gmail.com>
2024-04-28 19:29:50 +0900

    section 4 : 래퍼 클래스 - 문제와 풀이 및 정리

--- src/lang/clazz/ClassCreateMain.java
+++ src/lang/clazz/ClassCreateMain.java
@@ -4,6 +4,7 @@ public class ClassCreateMain {
 
     // 리플렉션 - reflection
     public static void main(String[] args) throws Exception {
+        
         Class helloClass = Hello.class;
         // Class helloClass = Class.forName("lang.clazz.Hello");
 
